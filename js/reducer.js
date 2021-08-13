a7674717c77d573ffa60e5b844bb407e7820d855
Lutful Kabir <lutfulkbr@yahoo.com>
2021-08-13 11:06:48 -0400

    commit tree 5925d270f7a587ba70cc7724b983784b6f9a4060

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,34 @@
 // add code snippets from README
+let button = document.getElementById('button')
+let button1 = document.getElementById('button1')
+let state
+
+function reducer(state = {count: 0}, action) {
+    switch(action.type) {
+        case 'INCREASE_COUNT':
+            return {count: state.count + 1}
+            
+        case 'DECREASE_COUNT':
+            return {count: state.count - 1}
+
+        default:
+            return state
+    }
+}
+
+function render() {
+    let container = document.getElementById('container')
+    container.textContent = state.count
+}
+
+function dispatch(action) {
+    state = reducer(state, action)
+    render()
+}
+
+button.addEventListener('click', () => dispatch({type: 'INCREASE_COUNT'}))
+button1.addEventListener('click', () => dispatch({type: 'DECREASE_COUNT'}))
+
+
+
+dispatch({type: '@@INIT'})
